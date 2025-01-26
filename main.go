b43dfbf04996384ca7c1363c91d4fb8e3849bd3c
Héctor <hector@HectorLaptop.local>
2025-01-26 15:56:53 +0100

    refactor assets

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ package main
 
 import (
 	"bytes"
-	_ "embed"
+	"embed"
 	"fmt"
 	"image"
 	"log"
@@ -25,11 +25,9 @@ import (
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
-//go:embed cursor.png
-var Cursor_png []byte
+//go:embed assets
+var commonAssets embed.FS
 
-//go:embed apexstatue1.png
-var Apexstatue1_png []byte
 
 const (
 	screenWidth  = 800
@@ -37,10 +35,13 @@ const (
 )
 
 type Game struct {
-	x      float64
-	y      float64
-	Cursor *Object
-	Statue *Object
+	x           float64
+	y           float64
+	Cursor      *Object
+	CursorHover *Object
+	Statue      *Object
+	IsHovering  bool
+	Background  *Object
 }
 
 type Object struct {
@@ -49,7 +50,7 @@ type Object struct {
 	drawDebugRect bool
 }
 
-func NewObject(img image.Image, x, y float64) *Object {
+func newObject(img image.Image, x, y float64) *Object {
 	return &Object{
 		x:   x,
 		y:   y,
@@ -69,14 +70,14 @@ func (o *Object) SetPosition(x, y float64) {
 func (o *Object) HitBy(other *Object) bool {
 	rect := image.Rect(int(o.x), int(o.y), int(o.x)+o.img.Bounds().Dx(), int(o.y)+o.img.Bounds().Dy())
 	otherRect := image.Rect(int(other.x), int(other.y), int(other.x)+other.img.Bounds().Dx(), int(other.y)+other.img.Bounds().Dy())
-	fmt.Println(rect, otherRect)
 	return rect.Overlaps(otherRect)
 }
 
-func (o *Object) Draw(screen *ebiten.Image) {
-	op := &ebiten.DrawImageOptions{}
-	op.GeoM.Translate(o.x, o.y)
-	screen.DrawImage(o.img, op)
+func (o *Object) Draw(screen *ebiten.Image, options *ebiten.DrawImageOptions) {
+	options.GeoM.Translate(o.x, o.y)
+	//options.GeoM.Scale(4.2, 4.2)
+
+	screen.DrawImage(o.img, options)
 
 	if o.drawDebugRect {
 		ebitenutil.DrawRect(screen, o.x, o.y, float64(o.img.Bounds().Dx()), float64(o.img.Bounds().Dy()), image.White)
@@ -94,42 +95,77 @@ func (g *Game) Update() error {
 	g.x = float64(x)
 	g.y = float64(y)
 
-	fmt.Println(g.Statue.HitBy(g.Cursor))
+	g.IsHovering = g.Statue.HitBy(g.Cursor)
 
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
+	emptyOp := ebiten.DrawImageOptions{}
+
+	scaleGeo := ebiten.GeoM{}
+	scaleGeo.Scale(4.2, 4.2)
+	opBackground := ebiten.DrawImageOptions{
+		GeoM: scaleGeo,
+	}
+	g.Background.Draw(screen, &opBackground)
 
 	//g.Statue.drawDebugRect = true
-	g.Statue.Draw(screen)
+	g.Statue.Draw(screen, &emptyOp)
 
 	//g.Cursor.drawDebugRect = true
-	g.Cursor.SetPosition(g.x-float64(g.Cursor.img.Bounds().Dx())/2, g.y-float64(g.Cursor.img.Bounds().Dy())/2)
-	g.Cursor.Draw(screen)
+	posCursorX := g.x - float64(g.Cursor.img.Bounds().Dx())/2
+	posCursorY := g.y - float64(g.Cursor.img.Bounds().Dy())/2
+
+	g.Cursor.SetPosition(posCursorX, posCursorY)
+	if g.IsHovering {
+		g.CursorHover.SetPosition(posCursorX, posCursorY)
+		g.CursorHover.Draw(screen, &emptyOp)
+	} else {
+		g.Cursor.Draw(screen, &emptyOp)
+	}
 
 	ebitenutil.DebugPrint(screen,
-		fmt.Sprintf("Move the redddd point by mouse wheeaal\n(%0.2f, %0.2f)", g.x, g.y))
+		fmt.Sprintf("DEBUG MESSAGES: %t", g.IsHovering))
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 	return screenWidth, screenHeight
 }
 
+func NewObjectFromSprite(blob []byte, x, y float64) *Object {
+	img, _, err := image.Decode(bytes.NewReader(blob))
+	if err != nil {
+		log.Fatal(err)
+	}
+	return newObject(img, x, y)
+}
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
 func main() {
-	img, _, err := image.Decode(bytes.NewReader(Cursor_png))
-	if err != nil {
-		log.Fatal(err)
-	}
-	cursor := NewObject(img, 0, 0)
 
-	img, _, err = image.Decode(bytes.NewReader(Apexstatue1_png))
-	if err != nil {
-		log.Fatal(err)
-	}
-	statue := NewObject(img, 100, 100)
+	Cursor_png, err := commonAssets.ReadFile("assets/cursor.png")
+	checkErr(err)
+	cursor := NewObjectFromSprite(Cursor_png, 0, 0)
 
-	g := &Game{x: 0.0, y: 0.0, Statue: statue, Cursor: cursor}
+	Cursor_hover_png, err := commonAssets.ReadFile("assets/cursorhover.png")
+	checkErr(err)
+	cursorHover := NewObjectFromSprite(Cursor_hover_png, 0, 0)
+
+	Apexstatue1_png, err := commonAssets.ReadFile("assets/apex/apexstatue1.png")
+	checkErr(err)
+	statue := NewObjectFromSprite(Apexstatue1_png, 100, 100)
+
+	Biome_png, err := commonAssets.ReadFile("assets/apex/bg1.png")
+	checkErr(err)
+	background := NewObjectFromSprite(Biome_png, 0, 0)
+
+	g := &Game{x: 0.0, y: 0.0, Statue: statue, Cursor: cursor, CursorHover: cursorHover, Background: background}
 
 	ebiten.SetVsyncEnabled(true)
 	ebiten.SetWindowSize(screenWidth, screenHeight)
